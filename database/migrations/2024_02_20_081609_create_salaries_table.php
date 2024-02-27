e99d8c074ead86855981019951effb94410b9cb7
RyanLama <parbattamang1234@gmail.com>
2024-02-27 11:24:52 +0545

    migrations fix

--- database/migrations/2024_02_20_081609_create_salaries_table.php
+++ database/migrations/2024_02_20_081609_create_salaries_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('salarys', function (Blueprint $table) {
+        Schema::create('salaries', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('u_id');
             $table->string('salary_type');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('salarys');
+        Schema::dropIfExists('salaries');
     }
 };
